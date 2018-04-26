3cc9efeb9f5a68e457092efce5f49b9cfed00df6
Grace Rhee <gracemrh@gmail.com>
2018-04-26 16:00:32 -0500

    fix firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,61 +11,6 @@ firebase.initializeApp(config);
 var dataRef = firebase.database();
 
 /**
- * FirebaseUI configuration
- */
-var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>'
-};
-
-/**
- * firebaseUI authentication
- */
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-  // Other config options...
-});
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-/**
- * render the FirebaseUI Auth interface
- * The start method will wait until the DOM is loaded.
- */
-// 
-ui.start('#firebaseui-auth-container', uiConfig);
-
-
-/**
  * 
  */
 // set the current time
