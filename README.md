738983b3ef5fade5ccd1766656a91d054091e164
kyhakw <kyhakw@hotmail.com>
2018-04-28 04:53:31 -0500

    add loggedIn page

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Train-Scheduler
+# Train Scheduler (Basic + challenge 1 & 3)
+
+### Overview
+
+#### Challenge 1
+Each train operates during the time between a designated first train departure time and last train operation time. The last operation time is defined in the constant LAST_TRAIN_ARRIVAL_TIME_FOR_A_DAY value. The "next train Arrival time" and the "minutes left" are updated/adjusted every minute accordingly. In otherwords during the time between the last train arrival and its first train departure time, trains do not operate. Currently the last train departure time is defined as "23:30."
+
+#### Challenge 3
+The user must login to run the application. The only authenticated users shall be allowed to open the web application and access the backend firebase. The user can login with her/his Google account or can sign in with other existing account.
+
+The appliation is in use of other third party framework or library, such as © firebase, Bootstrap, JQuery, moment.js, babel.js, Github.com.
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@
 };
 firebase.initializeApp(config);
 var dataRef = firebase.database();
+const LAST_TRAIN_ARRIVAL_TIME_FOR_A_DAY = "23:30";
 
 /**
  * 
@@ -16,11 +17,17 @@ var dataRef = firebase.database();
 // set the current time
 function updateTime() {
   $('#currentTime').empty();
-  var currentTime = `<strong>${moment().format('LLLL')}</strong> - current time ${moment().format('HH:mm:ss')}`;
+  var currentTime = `${moment().format('LLLL')} - current time <span class="bg-dark text-white"><strong>${moment().format('HH:mm')}</strong></span>`;
   $('#currentTime').append(currentTime);
   setTimeout(updateTime, 60000);
 }
 
+// clock displays every minute 
+var clockTickerId = setTimeout(updateTime, 0);
+
+/**
+ * function called to update the next arrival time and time left until the next train
+ */
 function updateScheduleEveryMinute() {
   var $scheduleTableBody = $("#scheduler tbody")
       .children()
@@ -32,11 +39,11 @@ function updateScheduleEveryMinute() {
         var nextArrival = $(tds[3]).text();
         var minutesAway = $(tds[4]).text();
         var currentTime = moment().format('HH:mm');
-
-        console.log("current time", currentTime);
+       
         frequency = parseInt(frequency);
         nextArrival = moment(nextArrival, 'HH:mm');
         minutesAway = parseInt(minutesAway);
+        console.log("current time", currentTime);
         console.log(frequency);
         console.log(nextArrival);
         console.log(minutesAway);
@@ -44,13 +51,16 @@ function updateScheduleEveryMinute() {
         // get the values that need to upate
         if (nextArrival.diff(moment(currentTime, 'HH:mm'), 'minutes') > 0) {
           console.log("cond 1");
-          $(tds[4]).text(minutesAway-1);
+          $(tds[4]).empty();
+          $(tds[4]).html(`<strong>${minutesAway-1}</strong>`);
         }
         else if (nextArrival.diff(moment(currentTime, 'HH:mm'), 'minutes') == 0) {
           console.log("cond 2");
-          //console.log(moment(nextArrival.add(frequency, 'minutes'), 'HH:mm').format('HH:mm'));
-          $(tds[3]).text(moment(nextArrival.add(frequency, 'minutes'), 'HH:mm').format('HH:mm'));
-          $(tds[4]).text(frequency);
+          $(tds[3]).empty();
+          $(tds[4]).empty();
+          var td3content = moment(nextArrival.add(frequency, 'minutes'), 'HH:mm').format('HH:mm');
+          $(tds[3]).html(`<strong>${td3content}</strong>`);
+          $(tds[4]).html(`<strong>${frequency}</strong>`);
         }
         else {
           console.log("this case is not supposed to happen");
@@ -71,68 +81,82 @@ $("#add-train").on("click", function(e) {
   var trainName = $("#trainName").val().trim();
   var destination = $("#destination").val().trim();
   var firstTrainTime = moment($("#firstTrainTime").val().trim(), "HH:mm").format("X");
-  //var firstTrainTime = moment($("#firstTrainTime").val().trim(), "HH:mm");
   var frequency = $("#frequency").val().trim();
   
   var newTrainSchedule = {
     trainName: trainName,
     destination: destination,
     firstTrainTime: parseInt(firstTrainTime),
-    frequency: frequency,
+    frequency: parseInt(frequency),
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   };
-
-  // console.log(trainName);
-  // console.log(destination);
-  // console.log(firstTrainTime);
-  // console.log(frequency);
 
   dataRef.ref().push(newTrainSchedule);
 });
 
 // Initial loader and firebase watcher. Note the difference fron .on("value")
 dataRef.ref().on("child_added", function(childSnap) {
+  console.log(childSnap.val());
 
   var trainName = childSnap.val().trainName;
   var destination = childSnap.val().destination;
   var firstTrainTime = childSnap.val().firstTrainTime;
   var frequency = childSnap.val().frequency;
 
-  var firstTrainTimeInFormat = moment.unix(firstTrainTime).format("HH:mm");
-  // console.log("firstTrainTime: " + firstTrainTime);
-  // console.log("firstTrainTimeInFormat : " + firstTrainTimeInFormat);
-  // console.log(moment().format('X'));
-  // console.log(moment.utc(moment(), 'HH:mm'));
+  var firstTrainTimeInFormat = moment.unix(firstTrainTime).format("HH:mm"); 
+  var currentTime = moment().format('HH:mm');
+ 
+  console.log("current time ", currentTime);
+  console.log("firstTrainTime: " + firstTrainTime);
+  console.log("firstTrainTimeInFormat : " + firstTrainTimeInFormat);
 
-  var timeDifference = moment.utc(moment(), 'HH:mm')
-                 .diff(moment.utc(firstTrainTimeInFormat, 'HH:mm'), "minutes");
+  /**
+   * Cuttent time of day is between the first train time and the last train arrival time that is 
+     * defined in the area at top of the main.js.
+   */
+  if (currentTime < firstTrainTimeInFormat || currentTime > LAST_TRAIN_ARRIVAL_TIME_FOR_A_DAY) {
 
-  //var timeDifference = 53;
-  var tRemainder = timeDifference % frequency;
-  var tMinutesAway = frequency - tRemainder;
-  var nextTrainArrival = moment().add(tMinutesAway, "minutes").format("HH:mm");
+    var tMinutesAway = moment(firstTrainTimeInFormat,"HH:mm").diff(moment(currentTime, "HH:mm"), "minutes");
+    var nextTrainArrival = firstTrainTimeInFormat;
 
-  $("#scheduler tbody").append("<tr><td>" + trainName + "</td>" +
+    $("#scheduler tbody").append("<tr><td>" + trainName + "</td>" +
                                     "<td>" + destination + "</td>" +
                                     "<td> every " + frequency + " min</td>" +
-                                    "<td>" + nextTrainArrival + "</td>" +
-                                    "<td>" + tMinutesAway + "</td></tr>");
+                                    "<td><strong>" + nextTrainArrival + "</strong></td>" +
+                                    "<td><strong>" + tMinutesAway + "</strong></td></tr>");
+  }
+  else {
+    /**
+     * Cuttent time of day is outside of the regular train operation hours (the first train time and 
+     * the lsat train arrival time that is defined in the area at top of the main.js).
+     */
+    var timeDifference = moment(moment(), "HH:mm").diff(moment(firstTrainTime, "X"), "minutes");
+    console.log(timeDifference);
+    
+    var tRemainder = timeDifference % frequency;
+    var tMinutesAway = frequency - tRemainder;
+    var nextTrainArrival = moment().add(tMinutesAway, "minutes").format("HH:mm");
 
-}, function(errorObject) {
+    $("#scheduler tbody").append("<tr><td>" + trainName + "</td>" +
+                                      "<td>" + destination + "</td>" +
+                                      "<td> every " + frequency + " min</td>" +
+                                      "<td><strong>" + nextTrainArrival + "</strong></td>" +
+                                      "<td><strong>" + tMinutesAway + "</strong></td></tr>");
+
+}}, function(errorObject) {
   console.log("Errors handled: " + errorObject.code);
 });
 
-dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snap) {
+// dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snap) {
 
-  $("#trainName").text(snap.val().trainName);
-  $("#destination").text(snap.val().destination);
-  $("#firstTrainTime").text(moment.unix(snap.val().firstTrainTime).format("HH:mm"));
-  $("#frequency").text(snap.val().frequency);
-})
+//   $("#trainName").text(snap.val().trainName);
+//   $("#destination").text(snap.val().destination);
+//   $("#firstTrainTime").text(moment.unix(snap.val().firstTrainTime).format("HH:mm"));
+//   $("#frequency").text(snap.val().frequency);
+// })
 
 
-// clock displays every minute 
-var clockTickerId = setTimeout(updateTime, 0);
+
 
 /**
  * challenge - 1
@@ -140,9 +164,6 @@ var clockTickerId = setTimeout(updateTime, 0);
  */
 var scheduleTickerId = setTimeout(updateScheduleEveryMinute, 60000);
 
-/**
- * user should be able to 
- */
 
 
 
